afe5ade3960d0085bda19955bdf7dbcb241edabd
Jonas <contact@atalantus.de>
2022-08-06 16:51:05 +0200

    Add shading

--- RayTracingTutorial/src/Renderer.cpp
+++ RayTracingTutorial/src/Renderer.cpp
@@ -1,65 +1,96 @@
 #include "Renderer.h"
 
+#include "Walnut/Random.h"
+
+namespace Utils
+{
+    static uint32_t ConvertToRGBA(const glm::vec4& color)
+    {
+        uint8_t r = (uint8_t)(color.r * 255.0f);
+        uint8_t g = (uint8_t)(color.g * 255.0f);
+        uint8_t b = (uint8_t)(color.b * 255.0f);
+        uint8_t a = (uint8_t)(color.a * 255.0f);
+
+        uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
+        return result;
+    }
+}
+
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
-	if (m_FinalImage)
-	{
-		if (m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height)
-			return;
+    if (m_FinalImage)
+    {
+        // No resize necessary
+        if (m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height)
+            return;
 
-		m_FinalImage->Resize(width, height);
-	}
-	else
-	{
-		m_FinalImage = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
-	}
+        m_FinalImage->Resize(width, height);
+    }
+    else
+    {
+        m_FinalImage = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
+    }
 
-	delete[] m_ImageData;
-	m_ImageData = new uint32_t[width * height];
+    delete[] m_ImageData;
+    m_ImageData = new uint32_t[width * height];
 }
 
 void Renderer::Render()
 {
-	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); ++y)
-		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); ++x)
-		{
-			glm::vec2 coord = {(float)x / m_FinalImage->GetWidth(), (float)y / m_FinalImage->GetHeight()};
-			coord = coord * 2.0f - 1.0f;
-			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
-		}
+    for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
+    {
+        for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
+        {
+            glm::vec2 coord = {(float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight()};
+            coord = coord * 2.0f - 1.0f; // -1 -> 1
 
+            glm::vec4 color = PerPixel(coord);
+            color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+            m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
+        }
+    }
 
-	m_FinalImage->SetData(m_ImageData);
+    m_FinalImage->SetData(m_ImageData);
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 {
-	glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
-	glm::vec3 rayDir(coord.x, coord.y, -1.0f);
-	float radius = 0.5f;
-	float lumninosity = 25.0f;
+    glm::vec3 rayOrigin(0.0f, 0.0f, 1.0f);
+    glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
+    float radius = 0.5f;
+    // rayDirection = glm::normalize(rayDirection);
 
+    // (bx^2 + by^2)t^2 + (2(axbx + ayby))t + (ax^2 + ay^2 - r^2) = 0
+    // where
+    // a = ray origin
+    // b = ray direction
+    // r = radius
+    // t = hit distance
 
-	rayDir = glm::normalize(rayDir);
+    float a = glm::dot(rayDirection, rayDirection);
+    float b = 2.0f * glm::dot(rayOrigin, rayDirection);
+    float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
 
-	float a = glm::dot(rayDir, rayDir);
-	float b = 2.0f * glm::dot(rayOrigin, rayDir);
-	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+    // Quadratic forumula discriminant:
+    // b^2 - 4ac
 
-	float sqrDiscr = b * b - 4.0f * a * c;
+    float discriminant = b * b - 4.0f * a * c;
+    if (discriminant < 0.0f)
+        return glm::vec4(0, 0, 0, 1);
 
-	if (sqrDiscr >= 0.0f)
-	{
-		float discr = sqrtf(sqrDiscr);
-		float t1 = (-b + discr) / (2.0f * a);
-		float t2 = (-b - discr) / (2.0f * a);
+    // Quadratic formula:
+    // (-b +- sqrt(discriminant)) / 2a
 
-		float t = fminf(t1, t2);
+    float closestT = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+    float t0 = (-b + glm::sqrt(discriminant)) / (2.0f * a); // Second hit distance (currently unused)
 
-		uint8_t brightness = (uint8_t)fminf(lumninosity / (4.0f * 3.14f * t * t) * 255.0f, 255.0f);
+    glm::vec3 hitPoint = rayOrigin + rayDirection * closestT;
+    glm::vec3 normal = glm::normalize(hitPoint);
 
-		return 0xff000000 | (brightness << 8) | brightness;
-	}
+    glm::vec3 lightDir = glm::normalize(glm::vec3(-1, -1, -1));
+    float lightIntensity = glm::max(glm::dot(normal, -lightDir), 0.0f); // == cos(angle)
 
-	return 0xffff0000;
+    glm::vec3 sphereColor(1, 0, 1);
+    sphereColor *= lightIntensity;
+    return glm::vec4(sphereColor, 1.0f);
 }
