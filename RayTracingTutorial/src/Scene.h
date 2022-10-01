65d09121ef1dcf9ed999fbf6d2a1efef1e05814f
Jonas <contact@atalantus.de>
2022-10-01 19:36:41 +0200

    render multiple spheres

---
+++ RayTracingTutorial/src/Scene.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <vector>
+
+struct Sphere
+{
+	glm::vec3 Position{0.0f};
+	float Radius = 0.5f;
+
+	glm::vec3 Albedo{1.0f};
+};
+
+struct Scene
+{
+	std::vector<Sphere> Spheres;
+};
