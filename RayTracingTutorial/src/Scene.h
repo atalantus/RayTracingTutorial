911d3640740839d329bbb2aafcde40feac526c60
Jonas Fritsch <jonas.fritsch@tum.de>
2023-07-07 18:36:43 +0200

    add emissive materials

--- RayTracingTutorial/src/Scene.h
+++ RayTracingTutorial/src/Scene.h
@@ -6,21 +6,25 @@
 
 struct Material
 {
-    glm::vec3 Albedo{1.0f};
-    float Roughness = 1.0f;
-    float Metallic = 0.0f;
+	glm::vec3 Albedo{1.0f};
+	float Roughness = 1.0f;
+	float Metallic = 0.0f;
+	glm::vec3 EmissionColor{0.0f};
+	float EmissionPower = 0.0f;
+
+	glm::vec3 GetEmission() const { return EmissionColor * EmissionPower; }
 };
 
 struct Sphere
 {
-    glm::vec3 Position{0.0f};
-    float Radius = 0.5f;
+	glm::vec3 Position{0.0f};
+	float Radius = 0.5f;
 
-    int MaterialIndex = 0;
+	int MaterialIndex = 0;
 };
 
 struct Scene
 {
-    std::vector<Sphere> Spheres;
-    std::vector<Material> Materials;
+	std::vector<Sphere> Spheres;
+	std::vector<Material> Materials;
 };
